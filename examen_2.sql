fff738210cf01a8a3075bb69386ecdaf92efc53f
Adrihanx <adrihanx@gmail.com>
2018-06-28 17:31:18 -0500

    Update examen_2.sql

--- examen_2.sql
+++ examen_2.sql
@@ -1,7 +1,7 @@
 create database examen_2;
   use examen_2;
   create table empleados(
-    id_empleado auto_increment primary key
+    id_empleado int auto_increment primary key
     ,nombre varchar(200)
     ,salario varchar(20)
     ,url_foto varchar(255)
